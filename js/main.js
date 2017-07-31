ecf3502d08f712869dcc27391c9920696a9f9be2
Sabit Huraira <sabitzhabit@gmail.com>
2017-07-31 23:07:16 +0700

    before publish

--- js/main.js
+++ js/main.js
@@ -44,27 +44,59 @@ var main = {
         var result=[];
         var back_btn=document.getElementById('back-btn');
         
-        firebase.database().ref('/datas/').once('value').then(function(datas) {
-        	var result_val=datas.val();
-        	var idx_data=0;
-        	for(var i=0;i<result_val.length;++i){
-        		if(result_val[i]!=null){
-	        		result.push({
-	        			'title': result_val[i].title,
-	        			'content': result_val[i].content
-	        		});
+        var ref = firebase.database().ref('/lucus/').orderByChild('title');
+    	tau.openPopup('#popup_toast');
+//        ref.once('value').then(function(datas) {
+//	        	var result_val=datas.val();
+//	        	var idx_data=0;
+//	        	for(var i=0;i<result_val.length;++i){
+//	        		if(result_val[i]!=null){
+//		        		result.push({
+//		        			'title': result_val[i].title,
+//		        			'content': result_val[i].content
+//		        		});
+//	
+//		        	    var li_form=document.createElement('li');
+//		        	    li_form.innerHTML = result_val[i].title;
+//		        	    li_form.setAttribute("class", "ui-li-static cerita-list");
+//		        	    li_form.setAttribute("id", "data"+idx_data);
+//		        	    list_data.appendChild(li_form);
+//		        	    
+//		        	    ++idx_data;
+//	        		}
+//	        	}
+//	        });
+    	
 
-	        	    var li_form=document.createElement('li');
-	        	    li_form.innerHTML = result_val[i].title;
-	        	    li_form.setAttribute("class", "ui-li-static cerita-list");
-	        	    li_form.setAttribute("id", "data"+idx_data);
-	        	    list_data.appendChild(li_form);
-	        	    
-	        	    ++idx_data;
-        		}
-        	}
-        	console.log(result);
-        });
+      var idx_data=0;
+      ref.on('child_added', function(datas) {
+    	var result_val=datas.val();
+//    	for(var i=0;i<result_val.length;++i){
+    		if(datas.val()!=null){
+        		result.push({
+        			'title': datas.val().title,
+        			'content': datas.val().content
+        		});
+
+    	  	    var li_form=document.createElement('li');
+    	  	    li_form.innerHTML = datas.val().title;
+    	  	    li_form.setAttribute("class", "ui-li-static cerita-list");
+    	  	    li_form.setAttribute("id", "data"+idx_data);
+    	  	    list_data.appendChild(li_form);
+    	  	    
+    	  	    ++idx_data;
+    		}
+//    	}
+    });
+        
+	        
+//	        ref.child('blogposts').child(id).once('value', function(snapshot) {
+//	        	  // The callback succeeded; do something with the final result.
+//	        	  renderBlog(snapshot.val());
+//	        	}, function(error) {
+//	        	  // The callback failed.
+//	        	  console.error(error);
+//	        	});
         
         back_btn.addEventListener('click', function onClick(){
         	tau.changePage('#one');
