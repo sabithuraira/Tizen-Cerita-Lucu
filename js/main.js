106f2bdae6a487e1ba0ace79800ef26965e1abaa
Sabit Huraira <sabitzhabit@gmail.com>
2017-08-04 18:16:41 +0700

    change using popup

--- js/main.js
+++ js/main.js
@@ -1,4 +1,78 @@
-(function () {
+(function () {   
+    var config = {
+    	    apiKey: "AIzaSyD8EaCZIliPuXSeitm86zaOkR1XxoWzbcQ",
+    	    authDomain: "cerita-lucu-2844d.firebaseapp.com",
+    	    databaseURL: "https://cerita-lucu-2844d.firebaseio.com",
+    	    projectId: "cerita-lucu-2844d",
+    	    storageBucket: "",
+    	    messagingSenderId: "1041028653269"
+    	  };
+    
+    var list_data = document.getElementById("list-data");
+    
+    var defaultApp = firebase.initializeApp(config);
+
+    var defaultDatabase = defaultApp.database();
+    defaultDatabase = firebase.database();
+    
+    var result=[];
+    
+    var ref = firebase.database().ref('/lucus/').orderByChild('title');
+	
+	var idx_data=0;
+  	ref.on('child_added', function(datas) {
+		if(datas.val()!=null){
+    		result.push({
+    			'title': datas.val().title,
+    			'content': datas.val().content
+    		});
+
+	  	    var li_form=document.createElement('li');
+	  	    li_form.innerHTML = datas.val().title;
+	  	    li_form.setAttribute("class", "ui-li-static cerita-list");
+	  	    li_form.setAttribute("id", "data"+idx_data);
+	  	    list_data.appendChild(li_form);
+	  	    
+	  	    ++idx_data;
+		}
+  	});
+    
+  	function settingList(){
+  		list_data.innerHTML="";
+
+   	 	var wk_form=document.createElement('li');
+  	    wk_form.innerHTML = "Welcome to wkwkwk land...";
+  	    wk_form.setAttribute("class", "ui-group-index");
+  	    list_data.appendChild(wk_form);	
+  		console.log(result);
+    	for(var i=0;i<result.length;++i){
+       	 	var li_form=document.createElement('li');
+	  	    li_form.innerHTML = result[i].title;
+	  	    li_form.setAttribute("class", "ui-li-static cerita-list");
+	  	    li_form.setAttribute("id", "data"+i);
+	  	    list_data.appendChild(li_form);	
+    	}
+  	}
+    
+    $('body').on('click', '.cerita-list',function(){
+    	var row_id = $(this).attr('id').substr(4);
+        var title_detail=document.getElementById('title-detail');
+        var content_detail=document.getElementById('content-detail');
+        
+        title_detail.innerHTML="";
+        title_detail.innerHTML=result[row_id].title;
+
+        content_detail.innerHTML="";
+        content_detail.innerHTML=result[row_id].content;
+        
+	    tau.openPopup("#btn_popup");
+    });
+    
+    $('#btn-close').on('click', function(ev)
+   {
+      tau.closePopup();
+   });
+	
 	window.addEventListener("tizenhwkey", function (ev) {
 		var activePopup = null,
 			page = null,
@@ -22,98 +96,3 @@
 }());
 
 
-var main = {
-    init: function init() {
-        'use strict';
-        
-        var config = {
-        	    apiKey: "AIzaSyD8EaCZIliPuXSeitm86zaOkR1XxoWzbcQ",
-        	    authDomain: "cerita-lucu-2844d.firebaseapp.com",
-        	    databaseURL: "https://cerita-lucu-2844d.firebaseio.com",
-        	    projectId: "cerita-lucu-2844d",
-        	    storageBucket: "",
-        	    messagingSenderId: "1041028653269"
-        	  };
-        
-        var list_data = document.getElementById("list-data");
-        var defaultApp = firebase.initializeApp(config);
-
-        var defaultDatabase = defaultApp.database();
-        defaultDatabase = firebase.database();
-        
-        var result=[];
-        var back_btn=document.getElementById('back-btn');
-        
-        var ref = firebase.database().ref('/lucus/').orderByChild('title');
-    	tau.openPopup('#popup_toast');
-//        ref.once('value').then(function(datas) {
-//	        	var result_val=datas.val();
-//	        	var idx_data=0;
-//	        	for(var i=0;i<result_val.length;++i){
-//	        		if(result_val[i]!=null){
-//		        		result.push({
-//		        			'title': result_val[i].title,
-//		        			'content': result_val[i].content
-//		        		});
-//	
-//		        	    var li_form=document.createElement('li');
-//		        	    li_form.innerHTML = result_val[i].title;
-//		        	    li_form.setAttribute("class", "ui-li-static cerita-list");
-//		        	    li_form.setAttribute("id", "data"+idx_data);
-//		        	    list_data.appendChild(li_form);
-//		        	    
-//		        	    ++idx_data;
-//	        		}
-//	        	}
-//	        });
-    	
-
-      var idx_data=0;
-      ref.on('child_added', function(datas) {
-    	var result_val=datas.val();
-//    	for(var i=0;i<result_val.length;++i){
-    		if(datas.val()!=null){
-        		result.push({
-        			'title': datas.val().title,
-        			'content': datas.val().content
-        		});
-
-    	  	    var li_form=document.createElement('li');
-    	  	    li_form.innerHTML = datas.val().title;
-    	  	    li_form.setAttribute("class", "ui-li-static cerita-list");
-    	  	    li_form.setAttribute("id", "data"+idx_data);
-    	  	    list_data.appendChild(li_form);
-    	  	    
-    	  	    ++idx_data;
-    		}
-//    	}
-    });
-        
-	        
-//	        ref.child('blogposts').child(id).once('value', function(snapshot) {
-//	        	  // The callback succeeded; do something with the final result.
-//	        	  renderBlog(snapshot.val());
-//	        	}, function(error) {
-//	        	  // The callback failed.
-//	        	  console.error(error);
-//	        	});
-        
-        back_btn.addEventListener('click', function onClick(){
-        	tau.changePage('#one');
-        });
-        
-        $('body').on('click', '.cerita-list',function(){
-        	var row_id = $(this).attr('id').substr(4);
-            var title_detail=document.getElementById('title-detail');
-            var content_detail=document.getElementById('content-detail');
-            
-            title_detail.innerHTML="";
-            title_detail.innerHTML=result[row_id].title;
-
-            content_detail.innerHTML="";
-            content_detail.innerHTML=result[row_id].content;
-            
-            tau.changePage('#detail');
-        });
-    },
-};
